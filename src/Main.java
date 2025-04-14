14dbdf32f0efe0f9c56c88cd0c2a57116112905d
Liam64k <liam.stier@gmail.com>
2025-04-13 21:13:06 -0400

    update #6 WORKING - Perlin Noise - Animation MOVING

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,26 @@ public class Main extends JFrame
     private static int _guiWidth = 1000;
     private static int _guiHeight = 400;
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         new Main();
     }
 
-    public Main()
+    public Main() throws Exception
     {
         getContentPane().setPreferredSize(new Dimension(_guiWidth, _guiHeight));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(null);
 
-        Terrain t = new Terrain(_guiWidth, _guiHeight, 70);
+        Terrain t = new Terrain(_guiWidth, _guiHeight, 10);
         System.out.println(Arrays.toString(t.getYArray()));
         t.setBounds(0, 0, _guiWidth, _guiHeight);
         add(t);
 
         pack(); // resizes JFrame to fit preferred size
         setVisible(true);
-        t.repaint();
+
+        Thread move = new Thread(t);
+        move.start();
     }
 }
--- src/Terrain.java
+++ src/Terrain.java
@@ -4,7 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 
-public class Terrain extends JPanel
+public class Terrain extends JPanel implements Runnable
 {
     private int _guiWidth;
     private int _guiHeight;
@@ -12,20 +12,34 @@ public class Terrain extends JPanel
     private static int _pointRadius = 3; // greater is less
     private static BufferedImage _perlinImage;
 
-    public Terrain(int guiWidth, int guiHeight, int resolution)
+    public Terrain(int guiWidth, int guiHeight, int resolution) throws Exception
     {
         _guiWidth = guiWidth;
         _guiHeight = guiHeight;
 
         _YArray = new int[_guiWidth/resolution];
 
-        try {
-            _perlinImage = ImageIO.read(new File("src/Perlin_noise_example.png"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        _perlinImage = ImageIO.read(new File("src/Perlin_noise_example.png"));
+        setYArray(0);
 
-        setYArray();
+    }
+
+    public void run()
+    {
+        try
+        {
+            int i = 0;
+            while (true)
+            {
+                repaint();
+                setYArray(i++);
+                Thread.sleep(50); // Slows down while loop so laptop doesn't crash
+            }
+        }
+        catch(Exception e)
+        {
+
+        }
 
     }
 
@@ -34,7 +48,7 @@ public class Terrain extends JPanel
         return _YArray;
     }
 
-    public void setYArray()
+    public void setYArray(int time)
     {
         int imgHeight = _perlinImage.getHeight();
         int imgWidth = _perlinImage.getWidth();
@@ -42,7 +56,7 @@ public class Terrain extends JPanel
         for (int i = 0; i < _YArray.length; i++)
         {
             int x = (i * imgWidth) / _YArray.length;
-            int gray = _perlinImage.getRGB(x, imgHeight / 3) & 0xFF;
+            int gray = _perlinImage.getRGB(x, time%imgHeight) & 0xFF; // time%imgHeight gets remainder to loop
             _YArray[i] = (_guiHeight * gray) / 255;
         }
     }
@@ -50,13 +64,16 @@ public class Terrain extends JPanel
     public void paintComponent(Graphics gui)
     {
         super.paintComponent(gui);
-        for (int i = 0; i < _YArray.length; i++)
+
+        /*
+        for (int i = 0; i < _YArray.length; i++) // draw vertices
         {
             int x1 = (i * _guiWidth) / (_YArray.length - 1);
             gui.drawOval(x1 - _pointRadius, _YArray[i] - _pointRadius, _pointRadius * 2, _pointRadius * 2);
         }
+        */
 
-        for (int i = 1; i <_YArray.length; i++)
+        for (int i = 1; i <_YArray.length; i++) // draw lines
         {
             int x1 = (i - 1) * _guiWidth / (_YArray.length - 1);
             int x2 = i * _guiWidth / (_YArray.length - 1);
